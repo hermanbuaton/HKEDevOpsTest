36e3154af955f8c5b6890254b0fdda3ebd78d5d5
hermanbuaton <hermanbuaton@gmail.com>
2018-12-19 08:07:47 +0000

    chinese

--- server.js
+++ server.js
@@ -20,18 +20,25 @@ var app = express();
 
 // Routes
 app.get('/', function (req, res) {
-  // res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write('./index.html');
-  // var now = new Date();
-  // res.send('Hello World! Today is ' + now.getFullYear() + "." + ( now.getMonth() + 1 ) + "." + now.getDate());
-});
-
-app.get('/chinese', function (req, res) {
+    
     fs.readFile('./index.html',function (err, data){
         res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
         res.write(data);
         res.end();
     });
+    // var now = new Date();
+    // res.send('Hello World! Today is ' + now.getFullYear() + "." + ( now.getMonth() + 1 ) + "." + now.getDate());
+    
+});
+
+app.get('/chinese', function (req, res) {
+    
+    fs.readFile('./index.html',function (err, data){
+        res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
+        res.write(data);
+        res.end();
+    });
+    
 });
 
 // Create the server
