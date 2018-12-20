eedb5c66eb87a2539d1769a54ee5974d764fdb5b
Herman BUATON <herman@kmleunghkelectric.onmicrosoft.com>
2018-12-20 08:13:37 +0000

    Call Backend services

--- server.js
+++ server.js
@@ -16,16 +16,29 @@ var iPortNum = 41085;
 
 var express = require('express');
 var fs = require('fs');
+var http = require('http');
+var exreq = require('request');
 var app = express();
 
 // Routes
 app.get('/', function (req, res) {
+
+    var result = "";
     
-    fs.readFile('./index.html',function (err, data){
-        res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
-        res.write(data);
-        res.end();
+    exreq('http://104.215.251.59:41299/api/func2/?param=dec20', function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            res.writeHead(200, {'Content-Type': 'text/html','Content-Length': body.length});
+            res.write(body);
+            res.end();
+        }
     });
+    
+    // fs.readFile('./index.html',function (err, data){
+    //     res.writeHead(200, {'Content-Type': 'text/html','Content-Length': result.length});
+    //     res.write(result);
+    //     res.end();
+    // });
+    
     // var now = new Date();
     // res.send('Hello World! Today is ' + now.getFullYear() + "." + ( now.getMonth() + 1 ) + "." + now.getDate());
     
